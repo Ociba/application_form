3ddaa431791b7f6c6945b5532290983a86aaa2cf
Ociba <ocibajames@gmail.com>
2024-12-04 12:03:31 +0300

    changed responsiveness

--- resources/views/livewire/start-application.blade.php
+++ resources/views/livewire/start-application.blade.php
@@ -269,21 +269,29 @@
                                                                 </div>
                                                             </div>
                                                             <div class="row mt-4">
-                                                                <div class="col-xxl-12 col-lg-12 col-sm-12 col-md-12">
-                                                                    <div class="row">
-                                                                      <div class="col-xxl-6 col-lg-6 col-sm-12 mb-2">
-                                                                           <i class="fa fa-star"></i> <strong>Trust Pilot</strong> <i class="fa fa-star bg-dark text-white p-1"></i> <i class="fa fa-star bg-dark text-white p-1"></i> <i class="fa fa-star bg-dark text-white p-1"></i> <i class="fa fa-star bg-dark text-white p-1"></i> <i class="fa fa-star bg-dark text-white p-1"></i> &nbsp; + 50,000 Reviews
-                                                                      </div>
-                                                                      <div class="col-lg-6 col-xxl-6 col-sm-12">
-                                                                            <button href="#" class="gradient-button btn-sm" type="submit" wire:loading.attr="disabled">
-                                                                            <span wire:loading wire:target="AddInfo">
-                                                                            <i class="fa fa-spinner fa-spin"></i>
-                                                                            </span>
-                                                                            <span wire:loading.remove wire:target="AddInfo">Start your appllication</span>
-                                                                            </button>
-                                                                        </div></div>
-                                                                </div>
-                                                            </div>
+    <div class="col-xxl-12 col-lg-12 col-sm-12 col-md-12">
+        <div class="row">
+            <div class="col-xxl-6 col-lg-6 col-sm-12 mb-2">
+                <i class="fa fa-star"></i> <strong>Trust Pilot</strong> 
+                <i class="fa fa-star bg-dark text-white p-1"></i>
+                <i class="fa fa-star bg-dark text-white p-1"></i>
+                <i class="fa fa-star bg-dark text-white p-1"></i>
+                <i class="fa fa-star bg-dark text-white p-1"></i>
+                <i class="fa fa-star bg-dark text-white p-1"></i> &nbsp; + 50,000 Reviews
+            </div>
+            <div class="col-lg-6 col-xxl-6 col-sm-12">
+                <!-- Ensure button spans full width on smaller devices -->
+                <button href="#" class="gradient-button btn-sm w-100" type="submit" wire:loading.attr="disabled">
+                    <span wire:loading wire:target="AddInfo">
+                        <i class="fa fa-spinner fa-spin"></i>
+                    </span>
+                    <span wire:loading.remove wire:target="AddInfo">Start your application</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
                                                     </form>
                                                 </div>
                                             </div>
